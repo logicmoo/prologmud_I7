8a294db11ff5afdb8190c3a15af7d690a94a2aa1
Douglas R. Miles <logicmoo@gmail.com>
2018-10-29 07:51:45 +0000

    tt0_00022_cycl.pl

---
+++ prolog/nldata/ac_xnl_iface.pl
@@ -0,0 +1,16 @@
+
+:- time(ensure_loaded(ac_xnl_7166)).
+/*
+   assertion_content/3
+   assertion_content/4
+   assertion_content/5
+   assertion_content/6
+   assertion_content/7
+   assertion_content/8
+   assertion_content/9
+   % assertion_content/10
+   assertion_content/11
+
+*/
+
+
